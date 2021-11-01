1e1a9c4895d1c823a3a60d092a90768c8ac1a2ab
Ferdinand Weinberger <w00lf04@protonmail.com>
2021-11-01 22:02:02 +0100

    Update interface.js

--- js/farmer/interface.js
+++ js/farmer/interface.js
@@ -206,14 +206,14 @@ function DrawPlanAmount(plan, targetNode, transactionsTargetnode){
                     window.planAmounts[plan] = new Number(window.planAmounts[plan]) + new Number(web3.utils.fromWei(deposit.amount));
                     window.planTransactions[plan] = window.planTransactions[plan] + "<div style=\"border-bottom:1px solid gray;\"><b>" + new Date(deposit.start*1000).toLocaleString() + "</b><br>" + currentAmount + " CAKE</div>";
 					
-					if( i+1 == depositCount){
+					//if( i+1 == depositCount){
 						targetNode.innerHTML = "Balance: " + amount + " CAKE";
 						
 						
 						if(transactionsTargetnode){
 							transactionsTargetnode.innerHTML = window.planTransactions[plan];
 						}
-					}
+					//}
                     
                     //console.log(new Date(deposit.start*1000), new Date(deposit.finish*1000), web3.utils.fromWei(deposit.amount), deposit.percent );
                 }
