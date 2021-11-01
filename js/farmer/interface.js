1e16fa68af42194e76de8f97418e6b902e320e10
Ferdinand Weinberger <w00lf04@protonmail.com>
2021-11-01 22:00:27 +0100

    Update interface.js

--- js/farmer/interface.js
+++ js/farmer/interface.js
@@ -201,7 +201,7 @@ function DrawPlanAmount(plan, targetNode, transactionsTargetnode){
                 if(deposit.plan == plan){
 					let amount = new Number(window.planAmounts[plan]).toFixed(3);
 					amount = amount.toLocaleString();
-					let currentAmount = new Number(web3.utils.fromWei(deposit.amount).toFixed(3).toLocaleString();
+					let currentAmount = new Number(web3.utils.fromWei(deposit.amount)).toFixed(3).toLocaleString();
 						
                     window.planAmounts[plan] = new Number(window.planAmounts[plan]) + new Number(web3.utils.fromWei(deposit.amount));
                     window.planTransactions[plan] = window.planTransactions[plan] + "<div style=\"border-bottom:1px solid gray;\"><b>" + new Date(deposit.start*1000).toLocaleString() + "</b><br>" + currentAmount + " CAKE</div>";
